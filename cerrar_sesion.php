cb6f3aee88ec19f3a35f33fbbb1b4995675120bf
oscarpt <oscarplate@gmail.com>
2018-11-09 16:19:23 +0100

    php litlechangres v2

--- cerrar_sesion.php
+++ cerrar_sesion.php
@@ -7,7 +7,7 @@ setcookie("c_email", $c_email, time() - (86400 * 90), "/"); // 86400 = 1 day
 setcookie("c_pass", $c_pass, time() - (86400 * 90), "/"); // 86400 = 1 day
 setcookie("last_con", $_SESSION["last_con"], time() - (86400 * 90), "/"); // 86400 = 1 day
 setcookie("style", $_SESSION["style"], time() - (86400 * 90), "/"); // 86400 = 1 day
-setcookie("flag_home", $_SESSION["flag_home"], time() - (86400 * 90), "/"); // 86400 = 1 day
+//setcookie("flag_home", $_SESSION["flag_home"], time() - (86400 * 90), "/"); // 86400 = 1 day
 
 session_unset();
 session_destroy();
