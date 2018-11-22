e8b7fe1c3a56304cf4bb3b1caabab919aff74ad5
AllRacs <oscarplate@gmail.com>
2018-11-22 12:42:44 +0100

    mi_perfil almost done

--- configuracion.php
+++ configuracion.php
@@ -19,10 +19,47 @@ if(isset($_SESSION["user"])){/*Si has iniciado sesion puedes ver esto*/
 <main>
     <h3>Configuracion</h3>
     <section>
-        <?php
-        
-
-         ?>
+        <div id="navegacion_perfil">
+            <?php
+            if(empty($_GET["user"]) && empty($_GET["pass"])){
+                $user = "";
+                $pass = "";
+            } else {
+                $user = $_GET["user"];
+                $pass = $_GET["pass"];
+            }
+                echo <<<END
+                    <label><button onclick="location.href='mi_perfil.php'" type="button">Mis Datos</button></label>
+                    <label><button onclick="location.href='configuracion.php'" type="button">Configuracion</button></label>
+                    <label><button onclick="location.href='mis_albumes.php'" type="button">Mis Álbumes</button></label>
+                    <label><button onclick="location.href='nuevo_album.php'" type="button">Crear Album</button></label>
+                    <label><button onclick="location.href='album_form_solicitud.php'" type="button">Solicitar Impresión de Album</button></label>
+                    <label><button onclick="location.href='anyadir_foto_album.php'" type="button">Añadir Foto a Album</button></label>
+                    <label><button onclick="location.href='baja_usuario.php'" type="button">Darme de Baja</button></label>
+END;
+            ?>
+        </div>
+        <div id="container_posting_perfil">
+            <?php
+            if(isset($_SESSION["user"])){
+                // echo '<p>User: '.$_SESSION["user"].'</p>';
+                echo '<p>Configuracion, elige el estilo de página que deseas usar:</p>';
+                //consusta a bd
+                $sentencia = 'SELECT * FROM Estilos';
+                if(!($resultado = $mysqli->query($sentencia))) {
+                  echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error;
+                  echo '</p>';
+                  exit;
+                }
+                //select > option para cada css
+                echo "<select>";
+                while($fila = $resultado->fetch_object()) {
+                  echo "<option>$fila->Fichero</option>";
+                }
+                echo "</select>";
+            }
+            ?>
+        </div>
     </section>
  </main>
  <?php
--- mi_perfil.php
+++ mi_perfil.php
@@ -30,7 +30,7 @@ if(isset($_SESSION["user"])){/*Si has iniciado sesion puedes ver esto*/
             }
                 echo <<<END
                     <label><button onclick="location.href='mi_perfil.php'" type="button">Mis Datos</button></label>
-                    <label><button onclick="location.href='configuracion.php'" type="button">Mis Datos</button></label>
+                    <label><button onclick="location.href='configuracion.php'" type="button">Configuracion</button></label>
                     <label><button onclick="location.href='mis_albumes.php'" type="button">Mis Álbumes</button></label>
                     <label><button onclick="location.href='nuevo_album.php'" type="button">Crear Album</button></label>
                     <label><button onclick="location.href='album_form_solicitud.php'" type="button">Solicitar Impresión de Album</button></label>
@@ -42,7 +42,31 @@ END;
         <div id="container_posting_perfil">
             <?php
             if(isset($_SESSION["user"])){
-                echo '<p>User: '.$_SESSION["user"].'</p>';
+                $sentencia = 'SELECT * FROM usuarios where Email like "'.$_SESSION["user"].'"';
+                if(!($resultado = $mysqli->query($sentencia))) {
+                    echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error;
+                    echo '</p>';
+                    exit;
+                }
+
+                $fila = $resultado->fetch_object();
+                echo '<p>Nombre: '.$fila->NomUsuario.'</p>';
+                //Render image
+                /*DATA IMAGE MIRAR PH1 DE 16-17*/
+                $foto = imagejpeg($fila->Foto);
+                echo '<p>Email: '.$fila->Email.'</p>';
+                echo '<p>Nacimiento: '.$fila->FNacimiento.'</p>';
+                echo '<p>Ciudad: '.$fila->Ciudad.'</p>';
+
+                $sentencia = 'SELECT NomPais FROM usuarios u, paises p where p.IdPais = u.Pais and u.Email like "'.$_SESSION["user"].'"';
+                if(!($resultado = $mysqli->query($sentencia))) {
+                    echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error;
+                    echo '</p>';
+                    exit;
+                }
+
+                $fila = $resultado->fetch_object();
+                echo '<p>Pais: '.$fila->NomPais.'</p>';
             } else {
                 echo '<p>Se muestran todas las fotos del user...</p>';
             }
