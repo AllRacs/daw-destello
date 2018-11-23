e8f7d4572c461878a6511037ae94fe43818361e1
AllRacs <oscarplate@gmail.com>
2018-11-23 16:52:03 +0100

    before php 8 correction

--- configuracion.php
+++ configuracion.php
@@ -10,7 +10,8 @@ include("sesionstart.php");
     }
 ?>
 <style>
-    <?php include 'CSS/main_mi_perfil.css';?>
+    <?php include 'CSS/main_mi_perfil.css';
+    include 'CSS/main_configuracion.css'?>
 </style>
 <?php
 
@@ -58,13 +59,84 @@ END;
                 }
                 echo "</select>";
             }
-            ?>
-        </div>
-    </section>
- </main>
- <?php
+
+            echo '<p>Detalles de usuario:</p>';
+            $sentencia2 = 'SELECT NomPais FROM Paises';
+            if(!($pais = $mysqli->query($sentencia2))) {
+              echo "<p>Error al ejecutar la sentencia <b>$sentencia2</b>: " . $mysqli->error;
+              echo '</p>';
+              exit;
+            }
+
+            $sentencia3 = 'SELECT * FROM usuarios u where u.Email like "'.$_SESSION["user"].'"';
+            if(!($usuu = $mysqli->query($sentencia3))) {
+              echo '<p>Error al ejecutar la sentencia <b>'.$sentencia3.'</b>: ' . $mysqli->error;
+              echo '</p>';
+              exit;
+            }
+            $usu = $usuu->fetch_object();
+            echo <<<ggg
+
+            <form id="form_registro" action="guardar_cambios.php" method="POST">
+                <!--Register_Form-->
+                <label for="user_img">
+                    <span class="hiddeofscreen">Profile Picture</span>
+                    <!--<span class="icon-file-image" aria-hidden="true"></span> User Picture REGISTER -->
+                    <img src="$usu->Foto" alt="">                    
+                </label>
+                <input id="user_img" type="file" name="register" value="Upload image">
+                <br>
+                <label for="input_name">
+                    <span class="hiddeofscreen">Usuario:</span>
+                    <span class="icon-user-circle-o" aria-hidden="true"></span>
+                </label><!-- User/email REGISTER -->
+                <input id="input_name" type="text" name="input_name" value="$usu->NomUsuario" placeholder="User Name" required>
+                <br>
+                <label for="input_pass">
+                    <span class="hiddeofscreen">Password:</span>
+                    <span class="icon-key" aria-hidden="true"></span><!-- Password REGISTER -->
+                </label>
+                <input id="input_pass" type="text" name="input_pass" value="$usu->Clave" placeholder="Password" required>
+                <br>
+                <label for="input_email">
+                    <span class="hiddeofscreen">Email:</span>
+                    <span class="icon-at" aria-hidden="true"></span><!-- E-mail REGISTER -->
+                </label>
+                <input id="input_email" type="email" name="input_email" value="$usu->Email" placeholder="E-mail" required>
+                <br>
+                <label for="input_calendar">
+                    <span class="hiddeofscreen">Birthday:</span>
+                    <span class="icon-calendar" aria-hidden="true"></span><!-- Birthday REGISTER -->
+                </label>
+                <input id="input_calendar" type="date" name="input_calendar" value="$usu->FNacimiento" placeholder="Birthday">
+                <br>
+                <label for="input_city">
+                    <span class="icon-home-outline" aria-hidden="true"></span><!-- Location REGISTER -->
+                </label>
+                <input id="input_city" type="text" name="input_city" value="$usu->Ciudad" placeholder="City">
+                <br>
+                <label for="input_country">
+                  <span>Country:</span>
+                  <select name="Country" id="input_country" required>
+
+ggg;
+            while($fila = $pais->fetch_assoc()){
+                echo'<option value="'. $fila['NomPais'] .'">'. $fila['NomPais'] .'</option>';
+            }
+            echo '</select>
+                        </label>
+                        <br>
+                        <!--Register_End-->
+                        <span>
+                            <button type="submit" name="input_submit" value="Guardar">Guardar</button>
+                        </span>
+                    </form>
+                    </div>
+                </section>
+             </main>';
+
  // Libera la memoria ocupada por el resultado
- $sentencia->close();
+ $resultado->close();
  // Cierra la conexión
  $mysqli->close();
  }else{/*Si no has iniciado sesion se te recomiendo iniciarla*/
