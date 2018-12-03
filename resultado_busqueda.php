cc0cda2557ce7c4401f5fb2374cf4a395ce90f75
AllRacs <oscarplate@gmail.com>
2018-12-03 10:57:27 +0100

    Fixin' The Search

--- resultado_busqueda.php
+++ resultado_busqueda.php
@@ -20,46 +20,72 @@ include("include/nav.inc");
     <?php
     /*PONERLO EN FORMA DE ETIQUEDAS, NO EN ENUMERACION*/
 
-    if(!empty($_GET['busqueda_top'])){
-        echo htmlspecialchars($_GET['busqueda_top']);
-    } else if(!empty($_GET)){
-        foreach ($_GET as $key => $value) {
-            if(!empty($value)){
-                echo '<li>'.$key.': '.$value.'</li>';
+    // if(!empty($_GET['busqueda_top'])){
+    //     echo '<p style="padding-left:6em">'.$_GET["busqueda_top"].'</p>';
+    // } else if(!empty($_GET)){
+    //     foreach ($_GET as $key => $value) {
+    //         if(!empty($value)){
+    //             echo '<li>'.$key.': '.$value.'</li>';
+    //
+    //             // $sentencia = 'SELECT titulo, Descripcion, Fecha, Pais, Fichero, Alternativo, Album, paises.NomPais
+    //             // FROM Fotos, Paises
+    //             // WHERE paises.NomPais LIKE ("'.$_GET["Country"].'") AND titulo LIKE ("'.$_GET["Title"].'")
+    //             // OR titulo LIKE ("'.$_GET["Title"].'")
+    //             // OR paises.NomPais LIKE ("'.$_GET["Country"].'")';
+    //             $sentencia = 'SELECT u.NomUsuario, f.Titulo, f.FRegistro, p.NomPais, f.Fichero, f.IdFoto FROM usuarios u JOIN albumes a JOIN fotos f JOIN paises p
+    //             WHERE u.IdUsuario = a.Usuario AND a.IdAlbum = f.Album AND f.Pais = p.IdPais';
+    //
+    //             if(true){
+    //                 //concatenacion de condicionales
+    //             }
+    //
+    //             $sentencia .='ORDER BY f.FRegistro DESC';
+    //
+    //             if(!($buscar = $mysqli->query($sentencia))) {
+    //                 echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error;
+    //                 echo '</p>';
+    //                 exit;
+    //             }
+    //         }
+    //
+    //
+    //         foreach ( $_GET["busqueda_avanzada"] as $busqueda_avanzada ) {
+    //             if(!empty($busqueda_avanzada)){
+    //                 echo $busqueda_avanzada;
+    //
+    //             }
+    //             if(!($busqueda_avanzada === end($_GET["busqueda_avanzada"]))){
+    //                 echo ", ";
+    //             }
+    //         }
+    //     }
+    //
+    // }
 
-                $sentencia = 'SELECT titulo, Descripcion, Fecha, Pais, Fichero, Alternativo, Album, paises.NomPais
-                FROM Fotos, Paises
-                WHERE paises.NomPais LIKE ("'.$_GET["Country"].'") AND titulo LIKE ("'.$_GET["Title"].'")
-                OR titulo LIKE ("'.$_GET["Title"].'")
-                OR paises.NomPais LIKE ("'.$_GET["Country"].'")
-                ';
-                if(!($buscar = $mysqli->query($sentencia))) {
-                    echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error;
-                    echo '</p>';
-                    exit;
-                }else{
-                    //Pruebas
-                }
-            }
+    if(!empty($_GET)){
+        $sentencia = 'SELECT u.NomUsuario, f.Titulo, f.FRegistro, p.NomPais, f.Fichero, f.IdFoto FROM usuarios u JOIN albumes a JOIN fotos f JOIN paises p
+        WHERE u.IdUsuario = a.Usuario AND a.IdAlbum = f.Album AND f.Pais = p.IdPais ';
 
-
-            foreach ( $_GET["busqueda_avanzada"] as $busqueda_avanzada ) {
-                if(!empty($busqueda_avanzada)){
-                    echo $busqueda_avanzada;
-
-                }
-                if(!($busqueda_avanzada === end($_GET["busqueda_avanzada"]))){
-                    echo ", ";
-                }
-            }
+        if(isset($_GET["busqueda_top"]) && !empty($_GET["busqueda_top"])){
+            //concatenacion de condicionales
+            echo '<p style="padding-left:6em">'.$_GET["busqueda_top"].'</p>';
+            $sentencia .=' AND f.Titulo LIKE "'.$_GET["busqueda_top"].'" ';
         }
 
+        $sentencia .=' ORDER BY f.FRegistro DESC';
+
+        if(!($buscar = $mysqli->query($sentencia))) {
+            echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error;
+            echo '</p>';
+            exit;
+        }
     }
 
     // Recorre el resultado y lo muestra en forma de tabla HTML
     while(isset($buscar) && $fila = $buscar->fetch_assoc()) {
         echo $fila->Titulo;
-        echo '<div class="container_posting">
+        echo <<<ggg
+        <div class="container_posting">
         <div class="p_box">
         <label class="title">$fila->Titulo</label>
         <span> - </span>
@@ -75,7 +101,8 @@ include("include/nav.inc");
         <label>$fila->NomUsuario</label>
         <time datetime="2018-10-01">$fila->FRegistro</time>
         </div>
-        </div>';
+        </div>
+ggg;
     }
     ?>
 
