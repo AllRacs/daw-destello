15ec7a3905268d0ac64f542a7bc13dfbfe3f439a
oscarpt <oscarplate@gmail.com>
2018-12-06 01:10:28 +0100

    Guardar el STYELO: Depurado de código

--- guardar_estilo_respuesta.php
+++ guardar_estilo_respuesta.php
@@ -18,27 +18,7 @@ include("include/nav.inc");
 <?php
 
 if(isset($_SESSION["user"])){/*Si has iniciado sesion puedes ver esto*/
-    // echo 'Página respuesta álbum, realizar la inserción en la DB aquí';
-    //Title=&Description=&Date=&Country=&photo=&Alternative=&Album=
-    // if(isset($_GET["estilo"])){
-    //     //UPDATE `usuarios` SET `Estilo` = '3' WHERE `usuarios`.`IdUsuario` = 1;
-    //     $sentencia = 'UPDATE usuarios SET Estilo = '.$_GET["estilo"].' WHERE usuarios.Email LIKE "'.$_SESSION["user"].'"';
-    //     echo $sentencia;
-    //     if(!($mysqli->query($sentencia))) {
-    //         echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error;
-    //         echo '</p>';
-    //         exit;
-    //     }
-    //     // if ($mysqli->query($sentencia) === TRUE) {
-    //     //     echo "New record created successfully";
-    //     // } else {
-    //     //     echo "Error: " . $sentencia . "<br>" . $mysqli->error;
-    //     // }
-    //
-    //     // Cierra la conexión con la base de datos
-    //     $mysqli->close();
-    // }
-    echo '<p style="padding-left:8em;padding-top:1em">El estilo se ha cambiado correctamente.</p>';
+    echo '<p style="padding-left:8em;padding-top:1em">El estilo se ha cambiado correctamente a '.$_SESSION["estilonombre"].'.</p>';
     echo '<a id="volver_al_perfil" href="mi_perfil.php">Volver al perfil</a>';
 } else { /*Si no has iniciado sesion se te recomiendo iniciarla*/
     echo '¡Vaya! parece que no estás loggeado <a href="registro.php">Accede ahora</a>';
